04d0064c870de762af1011244ef31a074d7b5b47
MrLitring <73461467+MrLitring@users.noreply.github.com>
2024-01-31 09:20:26 +0700

    first commit

--- SGUGIT_CourseWork/Forms/F0_MainLoad.Designer.cs
+++ SGUGIT_CourseWork/Forms/F0_MainLoad.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -38,11 +39,22 @@
             this.panel1.Size = new System.Drawing.Size(343, 504);
             this.panel1.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(408, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
             // F0_MainLoad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(221)))), ((int)(((byte)(248)))));
             this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Golos Text", 18F);
             this.ForeColor = System.Drawing.Color.Black;
@@ -52,10 +64,12 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "F0_MainLoad";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
