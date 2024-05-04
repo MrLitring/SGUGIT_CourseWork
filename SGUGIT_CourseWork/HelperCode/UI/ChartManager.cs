00f3f009b8d106b142a82f00330cc730c147e6c7
MrLitring <73461467+MrLitring@users.noreply.github.com>
2024-05-05 02:06:20 +0700

    Дописан ChartManager и переделан уровень 1

---
+++ SGUGIT_CourseWork/HelperCode/UI/ChartManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SGUGIT_CourseWork.HelperCode.UI
+{
+    internal class ChartManager
+    {
+        private Chart currentChart;
+        private SeriesChartType type;
+        private int widthBorder;
+        private string name;
+        private List<Series> seriesList;
+
+
+
+        public int roundX;
+        public int roundY;
+        public bool isStartToZero = true;
+
+
+
+        private ChartManager()
+        {
+            seriesList = new List<Series>();
+            widthBorder = 1;
+            roundX = 4;
+            roundY = 7;
+        }
+        public ChartManager(
+            Chart chart,
+            string name = "График",
+            SeriesChartType type = SeriesChartType.Spline) : this()
+        {
+            this.currentChart = chart;
+            this.name = name;
+            this.type = type;
+
+            chartDesigner();
+        }
+
+
+
+        public void Series_Add(string name)
+        {
+            Series series = new Series();
+            series.Name = name;
+            series.ChartType = type;
+            series.BorderWidth = widthBorder;
+
+            currentChart.Series.Add(series);
+            seriesList.Add(series);
+        }
+
+
+
+        private void AddPointXY(Series serie, double X, double Y)
+        {
+            if (serie == null) return;
+
+            X = Math.Round(X, roundX);
+            Y = Math.Round(Y, roundY);
+            serie.Points.AddXY(X, Y);
+        }
+        public void AddPointXY(string name, double X, double Y)
+        {
+            Series series = SerieSearch(name);
+
+            AddPointXY(series, X, Y);
+        }
+        public void AddPointXY(string name, List<double> X, List<double> Y)
+        {
+            Series serie = SerieSearch(name);
+            int min = Math.Min(X.Count, Y.Count);
+
+            for (int i = 0; i < min; i++)
+                AddPointXY(serie, X[i], Y[i]);
+        }
+
+        public void AddPointY(string name, double Y)
+        {
+            Series series = SerieSearch(name);
+            
+            AddPointXY(series, series.Points.Count + 1, Y);
+        }
+        public void AddPointY(string name, List<double> Y)
+        {
+            Series series = SerieSearch(name);
+
+            for (int i = 0; i < Y.Count; i++)
+                AddPointXY(series, i, Y[i]);
+        }
+
+        private void chartDesigner()
+        {
+            currentChart.Series.Clear();
+            currentChart.Titles.Clear();
+
+            currentChart.Titles.Add(name);
+            currentChart.ChartAreas[0].Axes[1].IsStartedFromZero = false;
+        }
+
+        private Series SerieSearch(string name)
+        {
+            foreach (Series serie in seriesList)
+            { if (serie.Name == name) return serie; }
+
+            return null;
+        }
+    }
+}
