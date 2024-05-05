3151d98ef1325dd8d51f15c759ea0698b4c701ad
MrLitring <73461467+MrLitring@users.noreply.github.com>
2024-05-05 12:46:36 +0700

    Переписан класс ChartManager и переписан под него Level_1

--- SGUGIT_CourseWork/Forms/P2_Level1.cs
+++ SGUGIT_CourseWork/Forms/P2_Level1.cs
@@ -66,8 +66,8 @@ namespace SGUGIT_CourseWork.Forms
             dataGridView.ColumnAdd("L<=E", work.LE, 4);
             dataGridView.ColumnAdd("LEs", work.LEs, 10);
 
-            ChartManager char_1 = new ChartManager(chart1,"Предельные значения функции S(t)");
-            ChartManager char_2 = new ChartManager(chart2, "Прогнозные функции для М(t)");
+            ChartManager char_1 = new ChartManager(chart1, checkedListBox1,"Предельные значения функции S(t)");
+            ChartManager char_2 = new ChartManager(chart2, checkedListBox2, "Прогнозные функции для М(t)");
             char_2.isStartToZero = false;
 
             char_1.Series_Add("M-");
