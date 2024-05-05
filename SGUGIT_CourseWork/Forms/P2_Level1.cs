e5d51d92b862502aea09d2acf1f1709855e54357
MrLitring <73461467+MrLitring@users.noreply.github.com>
2024-05-05 23:35:31 +0700

    Level2 Complite

--- SGUGIT_CourseWork/Forms/P2_Level1.cs
+++ SGUGIT_CourseWork/Forms/P2_Level1.cs
@@ -16,7 +16,7 @@ namespace SGUGIT_CourseWork.Forms
 {
     public partial class P2_Level1 : Form
     {
-        DataTable dTable = GeneralData.dataTable;
+        DataTable dTable;
 
 
 
@@ -24,51 +24,59 @@ namespace SGUGIT_CourseWork.Forms
         {
             InitializeComponent();
         }
-
-
-
-        private void Update()
+        public P2_Level1(DataTable dataTable) : this ()
         {
-            dataView.Rows.Clear();
-            dataView.Columns.Clear();
-            dTable.Clear(); 
-            dTable.Rows.Clear();
-            dTable.Columns.Clear();
-            dTable = GeneralData.dataTable;
-            MessageBox.Show("Uwu");
-
-            FillData();
+            dTable = dataTable;
         }
+
+
 
         private void P2_Level1_Load(object sender, EventArgs e)
         {
+            if(dTable == null) 
+            { 
+                dTable = GeneralData.dataTable; 
+            }
             FillData();
         }
 
         List<PointColumn> points = new List<PointColumn>();
         private void FillData()
         {
-            DataTableCalculation work = new DataTableCalculation(GeneralData.dataTable);
+            DataGridViewManager dataGridView = new DataGridViewManager(dataView);
+            ChartManager char_1 = new ChartManager(chart1, checkedListBox1);
+            ChartManager char_2 = new ChartManager(chart2, checkedListBox2, System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line);
+
+            char_1.TitleText = "Предельные значения функции S(t)";
+            char_2.TitleText = "Прогнозные функции для М(t)";
+            char_2.isStartToZero = false;
+
+            DataTable table = dTable.Clone();
+            for (int row = 0; row < dTable.Rows.Count; row++)
+            {
+                table.ImportRow(dTable.Rows[row]);
+            }
+            table.Columns.RemoveAt(0);
+            DataTableCalculation work = new DataTableCalculation(table);
+
             work.ColumnFill(false);
             work.Calculation();
 
-            DataGridViewManager dataGridView = new DataGridViewManager(dataView);
+            
             dataGridView.ColumnAdd("M-", work.columnMinus.responces, 4);
-            dataGridView.ColumnAdd("M", work.columnMinus.responces, 4);
+            dataGridView.ColumnAdd("M", work.columnNull.responces, 4);
             dataGridView.ColumnAdd("M+", work.columnPlus.responces, 4);
 
-            dataGridView.ColumnAdd("A-", work.columnMinus.responces, 4);
-            dataGridView.ColumnAdd("A", work.columnMinus.alphas, 4);
-            dataGridView.ColumnAdd("A+", work.columnPlus.responces, 4);
+            dataGridView.ColumnAdd("A-", work.columnMinus.alphas, 4);
+            dataGridView.ColumnAdd("A", work.columnNull.alphas, 4);
+            dataGridView.ColumnAdd("A+", work.columnPlus.alphas, 4);
 
             dataGridView.ColumnAdd("E", work.E, 10);
             dataGridView.ColumnAdd("L", work.L, 10);
             dataGridView.ColumnAdd("L<=E", work.LE, 4);
             dataGridView.ColumnAdd("LEs", work.LEs, 10);
 
-            ChartManager char_1 = new ChartManager(chart1, checkedListBox1,"Предельные значения функции S(t)");
-            ChartManager char_2 = new ChartManager(chart2, checkedListBox2, "Прогнозные функции для М(t)", System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line );
-            char_2.isStartToZero = false;
+            
 
             char_1.Series_Add("M");
             char_1.Series_Add("M-");
--- SGUGIT_CourseWork/Forms/P3_Level2.Designer.cs
+++ SGUGIT_CourseWork/Forms/P3_Level2.Designer.cs
@@ -28,11 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolSaveData = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.listBox1 = new System.Windows.Forms.ListBox();
@@ -41,7 +48,17 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.toolStrip1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -49,13 +66,26 @@
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tabControl1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripLabel1,
-            this.toolStripButton1,
+            this.toolSaveData,
             this.toolStripButton2});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
@@ -69,14 +99,15 @@
             this.toolStripLabel1.Size = new System.Drawing.Size(74, 22);
             this.toolStripLabel1.Text = "Сохранение";
             // 
-            // toolStripButton1
+            // toolSaveData
             // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = global::SGUGIT_CourseWork.Properties.Resources.Save;
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "Сохранить";
+            this.toolSaveData.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolSaveData.Image = global::SGUGIT_CourseWork.Properties.Resources.Save;
+            this.toolSaveData.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolSaveData.Name = "toolSaveData";
+            this.toolSaveData.Size = new System.Drawing.Size(23, 22);
+            this.toolSaveData.Text = "Сохранить";
+            this.toolSaveData.Click += new System.EventHandler(this.toolSaveData_Click);
             // 
             // toolStripButton2
             // 
@@ -86,9 +117,12 @@
             this.toolStripButton2.Name = "toolStripButton2";
             this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton2.Text = "Обновить";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Controls.Add(this.comboBox2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -96,6 +130,16 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 3);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(486, 21);
+            this.comboBox2.TabIndex = 0;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // tabPage1
             // 
@@ -164,6 +208,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(163, 21);
             this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // pictureBox1
             // 
@@ -186,6 +231,124 @@
             this.tabControl1.Size = new System.Drawing.Size(500, 375);
             this.tabControl1.TabIndex = 0;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(486, 322);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.splitContainer2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(486, 322);
+            this.panel2.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(486, 322);
+            this.splitContainer2.SplitterDistance = 123;
+            this.splitContainer2.SplitterWidth = 10;
+            this.splitContainer2.TabIndex = 2;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.chart1);
+            this.splitContainer3.Panel1.Controls.Add(this.checkedListBox1);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.chart2);
+            this.splitContainer3.Panel2.Controls.Add(this.checkedListBox2);
+            this.splitContainer3.Size = new System.Drawing.Size(486, 189);
+            this.splitContainer3.SplitterDistance = 249;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(249, 140);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 140);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(249, 49);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // chart2
+            // 
+            chartArea2.AxisY.IsStartedFromZero = false;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(0, 0);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(233, 140);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(0, 140);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(233, 49);
+            this.checkedListBox2.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(486, 123);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // P3_Level2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -199,6 +362,7 @@
             this.Load += new System.EventHandler(this.P3_Level2_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
@@ -207,6 +371,19 @@
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabControl1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,7 +392,7 @@
         #endregion
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolSaveData;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage1;
@@ -226,5 +403,15 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
     }
 }
